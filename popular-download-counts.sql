86843d4422a9df7201ecd96c775afcee07aff58f
heryanto <heryanto.tho@apps-foundry.com>
2015-10-30 16:24:38 +0700

    Fri Oct 30 16:24:38 WIB 2015

---
+++ popular-download-counts.sql
@@ -0,0 +1,11 @@
+﻿SELECT 
+	core_useractivities.item_id, 
+	count(*) AS download_count
+FROM 
+	core_useractivities 
+WHERE
+	core_useractivities.activity_type = 'download'
+	AND core_useractivities.download_status = 'success'
+	AND core_useractivities.datetime >= current_date - INTERVAL '2 month'
+GROUP BY 
+	core_useractivities.item_id
